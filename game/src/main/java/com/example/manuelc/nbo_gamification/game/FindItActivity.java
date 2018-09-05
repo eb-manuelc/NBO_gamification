5aa2be6e5bcca81986e6da92fa08b9536a832b80
eb-fcarabelli <fcarabelli@eventbrite.com>
2018-09-05 18:23:31 -0300

    First commit into find it

--- game/src/main/java/com/example/manuelc/nbo_gamification/game/FindItActivity.java
+++ game/src/main/java/com/example/manuelc/nbo_gamification/game/FindItActivity.java
@@ -1,7 +1,13 @@
 package com.example.manuelc.nbo_gamification.game;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -10,53 +16,147 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.webkit.URLUtil;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class FindItActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
+    private CameraSource cameraSource;
+    private SurfaceView cameraView;
+    private final int  MY_PERMISSION_REQUEST_CAMERA  = 1;
+    private String token = "";
+    private String tokenanterior = "";
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
+
     private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_it);
+        setContentView(R.layout.fragment_find_it);
+        cameraView = findViewById(R.id.camera_view);
+        initQR();
+    }
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    public void initQR(){
+
+        //Crear el detector QR - BarcodeDetector permite reconocer còdigos de barra y còdigos QR
+        final BarcodeDetector barcodeDetector= new BarcodeDetector.Builder(getApplicationContext())
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+
+        //Crear la camara fuente - Camera Source permite obtener frames de la camara del dispositivo y posteriormente analizarlo
+        cameraSource = new CameraSource.Builder(getApplicationContext(), barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .setAutoFocusEnabled(true)
+                .build();
+
+        //Listener del ciclo de vida de la camara
+        this.cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void surfaceCreated(SurfaceHolder holder) {
+                //Verificar si el usuario dio permisos para la camara
+
+                if(ActivityCompat.checkSelfPermission(FindItActivity.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED){
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                        if(shouldShowRequestPermissionRationale(Manifest.permission.CAMERA));
+                        requestPermissions(new String[]{Manifest.permission.CAMERA},
+                                MY_PERMISSION_REQUEST_CAMERA);
+                    }
+                    return;
+                }else{
+                    try{
+                        cameraSource.start(cameraView.getHolder());
+                    }catch (IOException ie){
+                        Log.e("CAMERA SOURCE", ie.getMessage());
+                    }
+                }
             }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if(barcodes.size() != 0){
+
+                    token = barcodes.valueAt(0).displayValue.toString();
+
+                    if(!token.equals(tokenanterior)){
+
+                        tokenanterior = token;
+                        Log.i("Token", token);
+
+                        if(URLUtil.isValidUrl(token)){
+                            //Si es una URL valida abre el navegador
+                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(token));
+                            startActivity(browserIntent);
+                        }else{
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(),token,Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try{
+                                synchronized (this){
+                                    wait(5000);
+                                    //limpiar el token
+                                    tokenanterior="";
+                                }
+
+                            }catch (InterruptedException e){
+                                Log.e("Error", "Waiting didn't work!");
+                                e.printStackTrace();
+                            }
+                        }
+                    }).start();
+                }
+            }
+
+
         });
 
     }
@@ -76,7 +176,6 @@ public class FindItActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -84,23 +183,13 @@ public class FindItActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
     public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
+
         private static final String ARG_SECTION_NUMBER = "section_number";
 
         public PlaceholderFragment() {
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
         public static PlaceholderFragment newInstance(int sectionNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
@@ -114,15 +203,10 @@ public class FindItActivity extends AppCompatActivity {
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_find_it, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
+           return rootView;
         }
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
